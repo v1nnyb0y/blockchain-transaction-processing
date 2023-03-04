488409a73ff583c7cb8d53ed1da073d3fe564970
Vitaly Ippolitov <vitaliy.ippolitov@convexitydmcc.com>
2023-03-04 18:09:52 +0400

    Metrics: implementation version 0

--- bknprocessing/node/src/main/kotlin/com/bknprocessing/node/controller/NodeController.kt
+++ bknprocessing/node/src/main/kotlin/com/bknprocessing/node/controller/NodeController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.system.measureTimeMillis
 
@@ -26,8 +27,15 @@ class NodeController(
 ) {
 
     private val log: Logger by logger()
-    var healthCheckAvg: AtomicLong = meterRegistry.gauge("health_check_avg", AtomicLong(0))!!
-    var verifyObjMetricGauge: AtomicLong = meterRegistry.gauge("verify_obj_metric_gauge", AtomicLong(0))!!
+
+    var healthCheckGauge: AvgValue = AvgValue(
+        value = meterRegistry.gauge("health_check_gauge_val", AtomicLong(0))!!,
+        count = meterRegistry.gauge("health_check_gauge_count", AtomicInteger(0))!!,
+    )
+    var verifyObjMetricAvg: AvgValue = AvgValue(
+        value = meterRegistry.gauge("verify_obj_gauge_val", AtomicLong(0))!!,
+        count = meterRegistry.gauge("verify_obj_gauge_count", AtomicInteger(0))!!,
+    )
 
     @PostMapping("/init")
     fun init(@RequestBody starterConfig: StarterConfig): String {
@@ -47,7 +55,14 @@ class NodeController(
         log.info("NodeController: verifyObj processed")
         measureTimeMillis {
             nodeService.verifyObj(obj)
-        }.also { verifyObjMetricGauge.set(it) }
+        }.also {
+            val oldCount = verifyObjMetricAvg.count.getAndIncrement()
+            if (oldCount == 0) {
+                verifyObjMetricAvg.value.set((verifyObjMetricAvg.value.get() + it) / 1)
+            } else {
+                verifyObjMetricAvg.value.set((verifyObjMetricAvg.value.get() + it) / oldCount)
+            }
+        }
     }
 
     @PostMapping("/verify")
@@ -68,13 +83,22 @@ class NodeController(
         nodeService.smartContract(obj)
     }
 
-    var modifyList = mutableListOf<Int>(25, 50, 75)
+    var index = 0
+    var modifyList = mutableListOf(25, 50, 75, 100, 0, 0)
+    // 25  37  50  62  50  41
 
     @Timed(description = "healthCheck_metric_timed", histogram = true)
     @GetMapping("/healthCheck")
     fun healthCheck(): String {
-        healthCheckAvg.set(modifyList[0].toLong())
-        modifyList.removeAt(0)
+        val oldCount = healthCheckGauge.count.getAndIncrement()
+        log.info("OldCount: $oldCount, healthCheckGauge.value: ${healthCheckGauge.value}")
+        if (oldCount == 0) {
+            healthCheckGauge.value.set((healthCheckGauge.value.get() + modifyList[0] + modifyList[index]) / 1)
+        } else {
+            healthCheckGauge.value.set((modifyList[0].toLong() + modifyList[index++]) / oldCount)
+        }
         return "Ok"
     }
 }
+
+data class AvgValue(val value: AtomicLong, val count: AtomicInteger)
